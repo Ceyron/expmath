951cd8171122743f3df1e031c23d4d729f50b62e
Felix Köhler <f.koehler@tum.de>
2019-09-14 15:30:41 +0200

    Improve commenting and add a fourth field

--- plots/multivariable_funktionen.py
+++ plots/multivariable_funktionen.py
@@ -14,10 +14,13 @@ def FUNC_2(X, Y):
     return X + Y
 def FUNC_3(X, Y):
     return x**2 + Y**2
+def FUNC_4(X, Y):
+    return np.sin(X/2)**2 + np.cos(Y/2)**2
 
 # Collecting all functions in a list of function pointers
-functions = [FUNC_1, FUNC_2, FUNC_3]
+functions = [FUNC_1, FUNC_2, FUNC_3, FUNC_4]
 
+# Similar to plotting with matplotlib or matlab
 x = np.linspace(-5, 5, 20)
 y = np.linspace(-5, 5, 20)
 X, Y = np.meshgrid(x, y)
@@ -26,8 +29,11 @@ def update_data(function_selector, surface_source):
     surface_source.data = {"X": X, "Y": Y, "Z": Z}
 
 
+# ColumnDataSource abstract the sending of new value pairs to the client
 surface_source = ColumnDataSource()
 
+# Uses an overridden version of the DOMlayout element togather with the visJS
+# library to enable a 3d scence
 surface = Surface3d(x="X", y="Y", z="Z", data_source=surface_source)
 
 # The employed 3d wrapper function seems to overlay the dropdown menus, there
@@ -36,18 +42,19 @@ surface = Surface3d(x="X", y="Y", z="Z", data_source=surface_source)
 spacing = Div(text="", width=5, height=600)
 
 function_selector = RadioButtonGroup(labels=["Funktion 1", "Funktion 2",
-        "Funktion 3"], active=0)
+        "Funktion 3", "Funktion 4"], active=0)
 
-inputs = widgetbox(function_selector)
 
-#update_data(function_selector, source)
-
+# Define callbacks
 def update_button(source):
     update_data(function_selector, surface_source)
 
+# Use callback in advane to populate the plot
 update_button(0)
 
-for button in (function_selector, ):
-    button.on_click(update_button)
+# Connect widgets with their respective callbacks
+function_selector.on_click(update_button)
 
+# Assemble the plot and create the html
+inputs = widgetbox(function_selector)
 curdoc().add_root(row(spacing, surface, inputs))
