2fd0d186b40deac7daa28f046c878b74881b5f2f
Felix Köhler <f.koehler@tum.de>
2019-10-27 12:01:56 +0100

    Revert to old variant

--- build_on_server.sh
+++ build_on_server.sh
@@ -1,12 +1,2 @@
-# Get the id of the previous expmath image so that it can be deleted later on
-line=$(sudo docker images | grep expmath)
-old_id=$(echo $line | cut -d " " -f3)
-
 # Build image
 sudo docker build -t expmath .
-
-# Delete old image only if building the new one was successfully
-if [[ $? == 0 ]]
-then
-    sudo docker image rm ${old_id}
-fi
