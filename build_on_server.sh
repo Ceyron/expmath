6d17660058f52a76404934459ac3301accfbec46
Felix Köhler <f.koehler@tum.de>
2019-10-27 11:38:16 +0100

    Old images will be deleted to save space

--- build_on_server.sh
+++ build_on_server.sh
@@ -1,1 +1,9 @@
+# Get the id of the previous expmath image so that it can be deleted later on
+line=$(sudo docker images | grep expmath)
+old_id=$(echo $a | cut -d " " -f3)
+
+# Build image
 sudo docker build -t expmath .
+
+# Delete old image
+sudo docker image rm ${old_id}
