1efd005b4a9033623a4242c1a7bd0e20b5231cf2
janinadierkes <56690063+janinadierkes@users.noreply.github.com>
2020-02-04 15:09:27 +0100

    Update diffbarkeit.py

--- plots/diffbarkeit.py
+++ plots/diffbarkeit.py
@@ -43,7 +43,7 @@ functions = [FUNC_1, FUNC_2, FUNC_3]
 
 
 def calculate_new_value_pairs(function_active):
-    if function_active == 1:
+    if function_active == 2:
         x_left = np.linspace(LEFT, - 0.0001, 100)
         x_right = np.linspace(0.0001, RIGHT, 100)
         x = np.concatenate([x_left_of_b, x_right_of_b])
